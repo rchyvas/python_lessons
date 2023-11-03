5caddc9489d7cb04a9fe62cb17a9c2a27b11982f
rchyvas <rchyvas@icloud.com>
2023-11-03 08:24:49 +0200

    initial commit

--- pandas.py
+++ pandas.py
@@ -3,7 +3,7 @@ import pandas as pd
 data = {
     "name": ["john", "arthur", "anna", "diana"],
     "age": [66, 23, 54, 19],
-    "city": ["vilnius", "klaipėda", "kaunas", "šilutė"]
+    "city": ["athens", "tallinn", "helsinki", "riga"]
 }
 
 df = pd.DataFrame(data)
